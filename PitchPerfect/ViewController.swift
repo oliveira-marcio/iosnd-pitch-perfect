edc4476598826a9a928ffc2bbfcd6f7756422f11
oliveira-marcio <emaildomso@gmail.com>
2020-04-13 18:12:30 +0100

    feat: Add stop button, label and view outlet

--- PitchPerfect/ViewController.swift
+++ PitchPerfect/ViewController.swift
@@ -10,14 +10,22 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var recordingLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
     @IBAction func recordAudio(_ sender: Any) {
-        print("Recording button was pressed.")
+        print("Recording button was pressed")
+        recordingLabel.text = "Recording in progress"
     }
+    
+    @IBAction func stopRecording(_ sender: Any) {
+        print("Stop Recording button was pressed")
+        recordingLabel.text = "Tap to record"
+    }
+    
 }
 
